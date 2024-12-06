bd546a55167188b4e3069045c6342755c80a3e22
Tom <spam@thomasbak.com>
2024-12-06 09:42:34 +0700

    5

--- 5.py
+++ 5.py
@@ -1,4 +1,6 @@
-USE_SAMPLE = True
+import itertools 
+
+USE_SAMPLE = False
 if USE_SAMPLE:
     data = """47|53
 97|13
@@ -32,15 +34,93 @@ else:
     with open('d5a.txt') as f:
         data = f.read()
         
-rules = []
+before_rules = {}
+after_rules = {}
 updates = []
 for line in data.splitlines():
     if line.find('|') != -1:
-        rules.append(list(map(int,line.split('|'))))
+        pages = list(map(int,line.split('|')))
+        if pages[0] not in before_rules:
+            before_rules[pages[0]] = []
+        before_rules[pages[0]].append(pages[1])
+
+        if pages[1] not in after_rules:
+            after_rules[pages[1]] = []
+        after_rules[pages[1]].append(pages[0])
+
     if line.find(',') != -1:
         updates.append(list(map(int,line.split(','))))
 
-print(rules)
+def test_update(update):
+    # did_pass = True
+    for before_index in range(len(update)):
+        for after_index in range(len(update)):
+            if before_index == after_index:
+                continue
+
+            before = update[before_index]
+            after = update[after_index]
+
+            if before_index < after_index:
+                if before not in before_rules or after not in before_rules[before]:
+                    # print('fail before {} {}'.format(before, after))
+                    # did_pass = False
+                    new_update = update[:]
+                    new_update[before_index] = update[after_index]
+                    new_update[after_index] = update[before_index]
+                    return (False, new_update)
+
+            if after_index < before_index:
+                if after in after_rules and before in after_rules[after]:
+                    # print('fail after {} {}'.format(before, after))
+                    # did_pass = False
+                    new_update = update[:]
+                    new_update[before_index] = update[after_index]
+                    new_update[after_index] = update[before_index]
+                    return (False, new_update)
+
+    # return did_pass
+    return (True, update)
+
+
+score = 0
+foobar = 0
+for update in updates:
+
+    print(update)
+    did_pass = test_update(update)[0]
+    print(did_pass)
+    
+    # Part 1
+    # if did_pass:
+        # score += update[ int((len(update)-1) /2) ]
+
+    # Part 2
+    if not did_pass:
+
+        while not did_pass:
+            (did_pass, new_update) = test_update(update)
+            if did_pass:
+                score += update[ int((len(new_update)-1) /2) ]
+                break
+            update = new_update
+
+        """
+        update_perms = itertools.permutations(update)
+        for update in update_perms:
+            did_pass = test_update(update)
+            if did_pass:
+                score += update[ int((len(update)-1) /2) ]
+                break
+        """
+print(score)
+exit()
+
+
+print(before_rules)
+print(after_rules)
 print(updates)
+
+print(score)
 
 
